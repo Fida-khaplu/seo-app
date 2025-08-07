3ebca05c1cd6c3a73c62d460b2ef8f31b78da838
Fida Hussain <fidakhaplu@gmail.com>
2025-08-07 12:01:27 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className=" text-5xl text-red-700">Hello WOrld</h1>
+    </div>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,8 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>Signup</h1>
+      
+    </div>
+  );
+}
